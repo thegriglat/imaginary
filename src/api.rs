f4392756a36a5b5c627fafbdf303cfa26e8070f5
Grigory Latyshev <thegriglat@gmail.com>
2023-10-17 23:44:26 +0300

    add query parameter

--- src/api.rs
+++ src/api.rs
@@ -1,6 +1,16 @@
-use actix_web::{get, HttpResponse, Responder};
+use actix_web::{get, web, HttpResponse, Responder};
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize)]
+pub struct QueryParams {
+    q: Option<String>,
+}
 
 #[get("/")]
-pub async fn handle_image() -> impl Responder {
-    HttpResponse::Ok().body("test")
+pub async fn handle_image(query: web::Query<QueryParams>) -> impl Responder {
+    let q = match query.q {
+        Some(ref q) => q,
+        None => "World",
+    };
+    HttpResponse::Ok().body(format!("Hello {}!", q))
 }
